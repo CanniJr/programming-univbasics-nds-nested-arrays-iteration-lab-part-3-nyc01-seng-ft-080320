3f40cd9d59b3142826c0b018b2566b395b725958
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 01:04:42 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,10 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   all_the_strings = []
   row_index = 0 
+  
   while row_index < src.count do
     element_index = 0 
-    while element_index < src.count[row_index] do
+    while element_index < src[row_index].count do
       if src[row_index][element_index].class == String
         all_the_strings << src[row_index][element_index]
         end
