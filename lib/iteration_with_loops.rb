558553e67a9b30a70a02207b981904aa5eb9d044
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-31 00:38:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,9 @@ def join_nested_strings(src)
       if src[row_index][element_index].class == String
         all_the_strings << src[row_index][element_index]
       end
-      element_index += 1 
+      all_the_strings.join("")
+    end
+    element_index += 1 
   end
   row_index += 1 
 end
